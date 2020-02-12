e07c52c913d2845447e67429ededb7b4a66d60f5
38704889@qq.com <38704889@qq.com>
2020-02-13 03:47:56 +0800

    modify

--- main.go
+++ main.go
@@ -19,8 +19,7 @@ func main() {
 	flag.StringVar(&imgUrl, "i", "", "img url")
 	flag.IntVar(&maxGo, "c", 15, "maximum number of goroutine")
 	flag.Parse()
-	//m3u8Url:="https://www.zbshuoqi.com:65/20190716/4Ik7g6nJ/index.m3u8"
-	//imgUrl = "http://sc.chinaz.com/tupian/"
+
 	switch {
 	case m3u8Url != "":
 		err := dowloadM3u8(m3u8Url)
