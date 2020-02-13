97dc04211677717c769d5c83b9778d88461e9753
38704889@qq.com <38704889@qq.com>
2020-02-14 02:20:45 +0800

    增加下载图片功能.

--- image.go
+++ image.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"time"
 )
+
 const (
 	//rePhone = `(1[3456789]\d)(\d{4})(\d{4})`
 	//reEmail = `[\w\.]+@\w+\.[a-z]{2,3}(\.[a-z]{2,3})?`
@@ -23,18 +24,18 @@ const (
 	relinkAll      = `<a[\s\S]+?href="([\s\S]+?)"` //正则取链接<a href=开头包含的数据.
 	reImg          = `<img([\s\S]+?)>`
 	reCharacterSet = `<meta.+?charset=(.+?)"`
-	TryImgNum = 2 //下载失败,再次重试下载次数.
+	TryImgNum      = 2 //下载失败,再次重试下载次数.
 )
 
 var (
-	imgWG sync.WaitGroup
-	imgSize        = 1024 * 30   //图片大于30K,才下载
+	imgWG          sync.WaitGroup
 	DowloadImgPath = "./img/"    //下载img图片保存路径.当前目录下的img目录.
 	linkUrls       = []string{}  //需要下载的所有URL
 	linkImgs       = sync.Map{}  //需要下载的图片信息
 	Tryimg         = []imgInfo{} //保存失败的下载,提供再次尝试下载
-	imgChan        chan *imgInfo  //并发下载图片文件的管道.
-	imgFailed      chan *imgInfo  //存放下载图片失败的管道
+	urlChan        chan string   //并发初始化所有url,提取要下载的图片链接
+	imgChan        chan *imgInfo //并发下载图片文件的管道.
+	imgFailed      chan *imgInfo //存放下载图片失败的管道
 )
 
 type imgInfo struct {
@@ -58,10 +59,16 @@ func DownloadImg(url string) {
 	fmt.Println("Download only pictures larger than 30K.")
 	//获得所有要下载的url网址
 	getUrlAll(url)
+
+	//初始化所有网址到管道
+	initUrlChan()
 	//并发下载所有网址的图片链接
-	getImgAll()
+	for i := 0; i < maxGo; i++ {
+		imgWG.Add(1)
+		go getUrlImgGo()
+	}
 	imgWG.Wait()
-    //初始化管道
+	//初始化管道
 	initImgChan()
 
 	//二.开始并发下载img文件,并发默认设为15,可以命令行调整.
@@ -72,7 +79,7 @@ func DownloadImg(url string) {
 	}
 	waitGroup.Wait()
 
-	//尝试重新下载失败的,TryNum=3再次重试3次.
+	//尝试重新下载失败的.
 	tryFailedImg()
 
 	cost := time.Since(start)
@@ -97,7 +104,7 @@ func DownloadImgGo() {
 		if err != nil {
 			img.isDownload = false
 			imgFailed <- img
-			fmt.Println("getUrlImg():", err)
+			//fmt.Println("getUrlResp():", err)
 			continue
 		}
 		defer resp.Body.Close()
@@ -129,31 +136,54 @@ func DownloadImgGo() {
 	}
 	waitGroup.Done()
 }
-//得到所要下载图片的页面.
-func getUrlAll(url string) {
-	//添加首页链接
-	linkUrls = append(linkUrls, url)
-	//添加首页下面一层的链接.
-	link := getUrlLink(url)
-	if len(link) == 0 {
-		return
+func getUrlNext(upper []string) (current []string) {
+	if MaxLayer == 0 {
+		return nil
 	}
-	for _, v := range link {
-		if isContains(linkUrls,v) == false{
-			linkUrls = append(linkUrls, v)
+	if len(upper) == 0 {
+		return nil
+	}
+	for _, v := range upper {
+		fmt.Println("init url: ", v)
+		link := getUrlLink(v)
+		if len(link) == 0 {
+			return
+		}
+		for _, v := range link {
+			if isContains(linkUrls, v) == false {
+				linkUrls = append(linkUrls, v)
+				current = append(current, v)
+			}
 		}
 	}
+	MaxLayer--
+	return getUrlNext(current)
 }
-//并发取得所有页面的图片链接
-func getImgAll(){
+
+//得到所要下载图片的页面.
+func getUrlAll(url string) {
+	//添加第一层链接
+	linkUrls = append(linkUrls, url)
+	//添加第二层的链接.
+	//current2 := getUrlNext(linkUrls)
+	getUrlNext(linkUrls)
+
+	////添加第三层的链接
+	//if len(current2) == 0 {
+	//	return
+	//}
+	//getUrlNext(current2)
+}
+func initUrlChan() {
+	urlChan = make(chan string, len(linkUrls))
 	for _, v := range linkUrls {
-		imgWG.Add(1)
-		go getUrlImg(v)
+		urlChan <- v
 	}
+	close(urlChan)
 }
 
 //根据正则取得的数据,进行筛选图片Url.
-func buildImg(value [][]string)(imgs []string) {
+func buildImg(value [][]string) (imgs []string) {
 	if len(value) == 0 {
 		return
 	}
@@ -162,9 +192,11 @@ func buildImg(value [][]string)(imgs []string) {
 
 	for i := 0; i < len(value); i++ {
 		t := value[i][1]
-		if strings.Contains(t, ".asp") ||strings.Contains(t, ".aspx") {
+		isUrl, _ := isUrlSuffix(t)
+		if isUrl == true {
 			continue
 		}
+
 		if strings.Contains(t, s1) {
 			i := strings.Index(t, s1)
 			t = t[i+len(s1):]
@@ -199,64 +231,82 @@ func buildImg(value [][]string)(imgs []string) {
 	return
 }
 
-//获取单个页面上的所有图片信息
-func getUrlImg(url string){
-	htmlText, err := getHtml(url)
-	if err != nil {
-		return
-	}
-	//如果没有换行,就增中换行
-	htmlText = strings.ReplaceAll(htmlText, "<", "\r\n<")
-	htmlText = strings.ReplaceAll(htmlText, ">", ">\r\n")
-	//根据reImg = `<img[\s\S]+?>` 正则提取数据
-	value := GetValueFromHtml(reImg, htmlText)
-	imgs := buildImg(value)
-	urls := buildUrl("img",url, imgs)
-	for _,v:=range urls{
- 		if strings.Index(v, ".js") > 0 {
+//并发取得所有页面的图片链接
+func getUrlImgGo() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("getUrlImgGo panic", err)
+			imgWG.Done()
+		}
+	}()
+
+	for url := range urlChan {
+		Resp, err := getUrlResp(url)
+		if err != nil {
+			//err = fmt.Errorf("getUrl():", err)
 			continue
 		}
-		v = strings.ReplaceAll(v, `"`, ``)
-		v = strings.ReplaceAll(v, "'", ``)
-		v = strings.TrimSpace(v)
-		//取得文件后缀
-		suffix := ".jpg"
-		m := strings.LastIndex(v, ".")
-		if m != -1 {
-			suffix = v[m:]
-			//有些网页在扩展名加一些其它字符.要去掉.
-			suffix = getImgSuffix(suffix)
+		defer Resp.Body.Close()
+		bytes, err := ioutil.ReadAll(Resp.Body)
+		if err != nil {
+			//err = fmt.Errorf("ioutil.ReadAll():", err)
+			continue
 		}
-		fileName := DowloadImgPath + GetRandomName() + suffix
+		htmlText := string(bytes)
 
-		img := imgInfo{
-			//index:      imgNum,
-			imgUrl:     v,
-			fileName:   fileName,
-			suffix:     suffix,
-			isDownload: false,
+		//如果没有换行,就增中换行
+		htmlText = strings.ReplaceAll(htmlText, "<", "\r\n<")
+		htmlText = strings.ReplaceAll(htmlText, ">", ">\r\n")
+		//根据reImg = `<img[\s\S]+?>` 正则提取数据
+		value := GetValueFromHtml(reImg, htmlText)
+		imgs := buildImg(value)
+		urls := buildUrl("img", url, imgs)
+		for _, v := range urls {
+			if strings.Index(v, ".js") > 0 {
+				continue
+			}
+			v = strings.ReplaceAll(v, `"`, ``)
+			v = strings.ReplaceAll(v, "'", ``)
+			v = strings.TrimSpace(v)
+			//取得文件后缀
+			suffix := ".jpg"
+			m := strings.LastIndex(v, ".")
+			if m != -1 {
+				suffix = v[m:]
+				//有些网页在扩展名加一些其它字符.要去掉.
+				suffix = getImgSuffix(suffix)
+			}
+			fileName := DowloadImgPath + GetRandomName() + suffix
+
+			img := imgInfo{
+				//index:      imgNum,
+				imgUrl:     v,
+				fileName:   fileName,
+				suffix:     suffix,
+				isDownload: false,
+			}
+			linkImgs.Store(v, &img)
+			fmt.Println("init img: ", url)
 		}
-		linkImgs.Store(v, &img)
-		fmt.Println("init url:",url)
 	}
 	imgWG.Done()
 }
+
 //初始化需要下载img的管道
 func initImgChan() {
-	i:=0
+	i := 0
 	linkImgs.Range(func(_, _ interface{}) bool {
 		i++
 		return true
 	})
-	imgChan = make(chan *imgInfo,i)
-	imgFailed = make(chan *imgInfo,i )
+	imgChan = make(chan *imgInfo, i)
+	imgFailed = make(chan *imgInfo, i)
 	linkImgs.Range(func(_, v interface{}) bool {
 		imgChan <- v.(*imgInfo)
 		return true
 	})
 	close(imgChan)
 }
-
 
 //获取url的源码,返回*http.Response
 func getUrlResp(url string) (resp *http.Response, err error) {
@@ -291,38 +341,74 @@ func getUrlHead(url string) (head string) {
 	return
 }
 
+//判断是否后缀是url
+func isUrlSuffix(str string) (isUrl bool, url string) {
+	//有些网页在扩展名加一些其它字符.要去掉.
+	m := strings.LastIndex(str, ".")
+	if m == -1 {
+		return false, ""
+	}
+	prefix := str[:m]
+	suffix := str[m:]
+	switch suffix {
+	case ".html":
+		suffix = ".html"
+	case ".htm":
+		suffix = ".htm"
+	case ".asp":
+		suffix = ".asp"
+	case ".aspx":
+		suffix = ".aspx"
+	case ".php":
+		suffix = ".php"
+	case ".jsp":
+		suffix = ".jsp"
+	case ".shtml":
+		suffix = ".shtml"
+	case ".shtm":
+		suffix = ".shtm"
+	default:
+		return false, ""
+	}
+	url = prefix + suffix
+	return true, url
+}
+
 //构建URL
-func buildUrl(tag string,masterUrl string,value []string)(url [] string) {
+func buildUrl(tag string, masterUrl string, value []string) (url []string) {
 	if len(value) == 0 {
 		return
 	}
+	url = []string{}
 	head := getUrlHead(masterUrl)
 	host := getHost(masterUrl)
 	for i := 0; i < len(value); i++ {
 		t := value[i]
 		t = strings.TrimSpace(t)
-		if t=="" {
+		if t == "" {
 			continue
 		}
 		if len(t) < 5 {
 			continue
 		}
-		if tag =="url"{
-			if !strings.Contains(t, ".htm") {
+		//构建针对url的情况特殊处理．(不处理img的链接情况)
+		if tag == "url" {
+			isUrl, s := isUrlSuffix(t)
+			if isUrl == false {
 				continue
 			}
+				t = s
 		}
-
 		//img链接,根据情况添加链接的头.
 		if strings.Contains(t, "http") || strings.Contains(t, "HTTP") {
 			//判断是否有重复的URL
-			if isContains(url, t) == false{
+			if isContains(url, t) == false {
 				url = append(url, t)
 			}
 			continue
 		}
 		if strings.Contains(t, `//`) {
-			if isContains(url, head+t) == false{
+			if isContains(url, head+t) == false {
 				url = append(url, head+t)
 			}
 			continue
@@ -336,13 +422,13 @@ func buildUrl(tag string,masterUrl string,value []string)(url [] string) {
 		//判断前缀是否有/
 		if !strings.HasPrefix(t, "/") {
 			if path != "" {
-				if isContains(url, path+t) == false{
+				if isContains(url, path+t) == false {
 					url = append(url, path+t)
 				}
 			}
 		} else {
 			if host != "" {
-				if isContains(url, host+t) == false{
+				if isContains(url, host+t) == false {
 					url = append(url, host+t)
 				}
 			}
@@ -352,7 +438,7 @@ func buildUrl(tag string,masterUrl string,value []string)(url [] string) {
 	return
 }
 
-//获取首网页下的URL链接网址.
+//获取当前网页下的URL链接网址.
 func getUrlLink(url string) (link []string) {
 	//获取当前页面下的所以链接.
 	HtmlText, err := getHtml(url)
@@ -364,15 +450,16 @@ func getUrlLink(url string) (link []string) {
 	HtmlText = strings.ReplaceAll(HtmlText, ">", ">\r\n")
 	//根据relinkALL正则提取数据
 	value := GetValueFromHtml(relinkAll, HtmlText)
-	temp :=make([]string,len(value))
-	for i,v:= range value{
-		temp[i]= v[1]
+	temp := make([]string, len(value))
+	for i, v := range value {
+		temp[i] = v[1]
 	}
 	//从正则匹配到的数据中,构建url链接
-	link = buildUrl("url",url,temp)
+	link = buildUrl("url", url, temp)
 	return
 }
-//取img后缀,因为有些图片链接网址,故意在后面加一些无关的字符.
+
+//取img后缀,因为有些图片链接网址,会在后面加一些无关的字符.要去掉.
 func getImgSuffix(s string) (v string) {
 	n := strings.LastIndex(s, ".")
 	if n == -1 {
@@ -400,6 +487,7 @@ func cutUrl(s string, substr string) string {
 	}
 	return s
 }
+
 //尝试重新下载n次失败的.
 func tryFailedImg() {
 	close(imgFailed)
@@ -426,7 +514,7 @@ func tryFailedImg() {
 			resp, err := getUrlResp(img.imgUrl)
 			if err != nil {
 				Tryimg[n].isDownload = false
-				//fmt.Println("getUrlImg():", err)
+				//fmt.Println("getUrlResp():", err)
 				continue
 			}
 			defer resp.Body.Close()
--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 )
 
 var (
-	maxGo     int = 15  //最大并发数
+	maxGo     int = 15        //最大并发数
+	imgSize   int = 1024 * 30 //图片大于30K,才下载
+	MaxLayer  int = 3         //查找网页的最大层数,用于图片下载
 	waitGroup sync.WaitGroup
-	m3u8Url   string  //m3u8下载的URL
-	imgUrl    string  //img下载的URL
+	m3u8Url   string //m3u8下载的URL
+	imgUrl    string //img下载的URL
 )
 
 func main() {
@@ -18,6 +20,8 @@ func main() {
 	flag.StringVar(&m3u8Url, "m", "", "M3U8 url index file")
 	flag.StringVar(&imgUrl, "i", "", "img url")
 	flag.IntVar(&maxGo, "c", 15, "maximum number of goroutine")
+	flag.IntVar(&imgSize, "s", 30, "Only download the above pictures (KB)")
+	flag.IntVar(&MaxLayer, "l", 3, "Download page Max Layer")
 	flag.Parse()
 
 	switch {
@@ -28,10 +32,11 @@ func main() {
 			return
 		}
 	case imgUrl != "":
+		imgSize = 1024 * imgSize
 		DownloadImg(imgUrl)
 
 	default:
-		fmt.Println(`The parameter (m or i) "M3U8 or Img url" must be entered.`)
+		fmt.Println(`The parameter (m  "M3U8 URL") or (i "Img url") must be entered.`)
 		return
 	}
 
